1c8791601f62664eaf4fd5bf8935d261cca4cf61
lasayuri <61148541+lasayuri@users.noreply.github.com>
2021-01-17 00:26:31 -0300

    road crossing game

---
+++ ator.js
@@ -0,0 +1,63 @@
+//variáveis ator
+let xAtor = 85;
+let yAtor = 366;
+let colisao = false;
+let meusPontos = 0;
+
+function mostraAtor(){
+  image(imagemDoAtor, xAtor, yAtor, 30, 30); //segundo parametro: posição x, terceiro: posição y, quarto: comprimento da imagem, quinto: altura imagem
+}
+
+function movimentaAtor(){
+  if (keyIsDown(UP_ARROW)){
+    yAtor -= 3;
+  }
+  
+  if (keyIsDown(DOWN_ARROW)){
+    if (podeSeMover()){
+      yAtor += 3;
+    }
+  }
+}
+
+function verificaColisao(){
+  //collideRectCircle(x1, y1, width1, height1, cx, cy, diameter)
+  for (let i = 0; i < imagemCarros.length; i++){
+    colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15);
+    
+    if (colisao){
+      voltaAtorParaPosicaoInicial();
+      somDaColisao.play();
+      if(pontosMaiorQueZero()){
+        meusPontos--;
+      }
+    }
+  }
+}
+
+function voltaAtorParaPosicaoInicial(){
+  yAtor = 366;
+}
+
+function incluiPontos(){
+  textAlign(CENTER);
+  textSize(25);
+  fill(color(255, 240, 60)); //cor varia de 0 a 255
+  text(meusPontos, width / 5, 27); 
+}
+
+function marcaPonto(){
+  if (yAtor < 15){
+    meusPontos++;
+    somDoPonto.play();
+    voltaAtorParaPosicaoInicial();
+  }
+}
+
+function pontosMaiorQueZero(){
+  return meusPontos > 0;
+}
+
+function podeSeMover(){
+  return yAtor < 368;
+}
---
+++ carro.js
@@ -0,0 +1,43 @@
+//variáveis do carro
+
+let xCarros = [600, 600, 600, 600, 600, 600];
+let yCarros = [40, 96, 150, 210, 270, 318]; //lista
+let velocidadeCarros = [2, 2.5, 3.2, 5, 3.3, 2.3];
+let comprimentoCarro = 50;
+let alturaCarro = 40;
+
+function mostraCarro(){  
+  for (let i = 0; i < imagemCarros.length; i++){    
+  image(imagemCarros[i], xCarros[i], yCarros[i], comprimentoCarro, alturaCarro);
+  }
+}
+
+function movimentaCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    xCarros[i] -= velocidadeCarros[i];
+  }
+}
+
+function voltaPosicaoInicialDoCarro(){
+  /*if (xCarros[0] < - 50){
+    xCarros[0] = 600;
+  }
+  
+  if (xCarros[1] < -50) {
+    xCarros[1] = 600;
+  }
+  
+  if (xCarros[2] < -50){
+    xCarros[2] = 600;
+  }*/
+  
+  for(let i = 0; i < imagemCarros.length; i++){
+    if (passouTodaATela(xCarros[i])){
+    xCarros[i] = 600;
+    }
+  }
+}
+
+function passouTodaATela(xCarro){
+  return xCarro < - 50;
+}
